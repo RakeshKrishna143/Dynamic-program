49fce873e9f042347dc6784ce315771059b52689
RakeshKrishna143 <41262164+RakeshKrishna143@users.noreply.github.com>
2019-08-07 16:55:56 +0530

    Update combination.py

--- combination.py
+++ combination.py
@@ -2,13 +2,13 @@ target=3
 a=['h','t']
 
 def dfs(a,target,index,path,res):
-    if len(path)==3:
+    if len(path)==target:
         res.append(path)
         return 
     
     else:
         for i in range(index,len(a)):
-            dfs(a,target-1,0,path+a[i],res)
+            dfs(a,target,0,path+a[i],res)
 
 def cs(a,target):
     res=[]
