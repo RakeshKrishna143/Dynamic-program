d9a61e74372e09ca0c2e7a1ec3ac31599b85815c
RakeshKrishna143 <41262164+RakeshKrishna143@users.noreply.github.com>
2019-12-25 23:05:53 +0530

    Update combination.py

--- combination.py
+++ combination.py
@@ -9,6 +9,9 @@ def dfs(a,target,index,path,res):
     else:
         for i in range(index,len(a)):
             dfs(a,target,0,path+a[i],res)
+            #dfs(a,target,i,path+a[i],res)
+            #dfs(a,target,i+1,path+a[i],res)
+
 
 def cs(a,target):
     res=[]
